a74d4ab6726a87a5aa1db9a2c48cf9632d59de33
xiaodeng <927561237@qq.com>
2021-10-24 21:40:01 +0800

    throw抛出异常改变状态

--- 手写Promise/promise.js
+++ 手写Promise/promise.js
@@ -18,8 +18,13 @@ function DwjPromise(executor) {
     this.PromiseResult = data
   }
 
-  // 同步调用 executor
-  executor(resolve.bind(this), reject.bind(this)) // 这里 resolve 和 reject 是直接调用的，所以 this 的指向是 window 要改变 this 的指向
+  try {
+    // 同步调用 executor
+    executor(resolve.bind(this), reject.bind(this)) // 这里 resolve 和 reject 是直接调用的，所以 this 的指向是 window 要改变 this 的指向
+  } catch (error) {
+    // throw 抛出异常改变状态
+    reject(error)
+  }
 }
 
 // 添加 then 方法
