401e206fa49f367faa6649223192981f5113a61f
xiaodeng <927561237@qq.com>
2021-10-24 21:30:29 +0800

    resolve和reject实现

--- 手写Promise/promise.js
+++ 手写Promise/promise.js
@@ -1,12 +1,25 @@
 function DwjPromise(executor) {
+  this.PromiseState = 'pending'
+  this.PromiseResult = null
+
   // resolve 函数
-  function resolve(data) {}
+  function resolve(data) {
+    // 修改对象的状态
+    this.PromiseState = 'fulfilled'
+    // 修改对象结果值
+    this.PromiseResult = data
+  }
 
   // reject 函数
-  function reject(data) {}
+  function reject(data) {
+    // 修改对象的状态
+    this.PromiseState = 'rejected'
+    // 修改对象结果值
+    this.PromiseResult = data
+  }
 
   // 同步调用 executor
-  executor(resolve, reject)
+  executor(resolve.bind(this), reject.bind(this)) // 这里 resolve 和 reject 是直接调用的，所以 this 的指向是 window 要改变 this 的指向
 }
 
 // 添加 then 方法
